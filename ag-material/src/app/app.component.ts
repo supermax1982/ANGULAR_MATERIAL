ebf26a277931260e5e2a70feb0060885ccdbd3f7
maxquaro <m.quaroni1982@gmail.com>
2020-02-23 05:01:41 +0100

    primo commit

---
+++ ag-material/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import {FormControl} from '@angular/forms';
+import {Observable} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'ag-material';
+  myControl = new FormControl();
+  options: string[] = ['One', 'Two', 'Three'];
+}
